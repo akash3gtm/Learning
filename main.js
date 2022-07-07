5de9e23627172adca56759a20b927aa77ee54d48
Akash Gautam <akashgautam@geekyants.com>
2022-07-07 10:48:29 +0530

    to do list completed

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ window.addEventListener('load', () => {
     const form = document.querySelector("#new-task-form");
     const input = document.querySelector("#new-task-input");
     const list_el = document.querySelector("#task");
+    const list_cl = document.querySelector("#tasks");
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -16,11 +17,7 @@ window.addEventListener('load', () => {
 
         tasks_el.appendChild(tasks_list_el);
 
-        const tasks_input_el =document.createElement("input");
-        tasks_input_el.classList.add("text");
-        tasks_input_el.type = "text";
-        tasks_input_el.value = tasks;
-        tasks_input_el.setAttribute("readonly", "readonly");
+        const tasks_input_el = create(tasks);
 
         tasks_list_el.appendChild(tasks_input_el);
 
@@ -40,13 +37,12 @@ window.addEventListener('load', () => {
         list_el.appendChild(tasks_el);
 
         tasks_edit_el.addEventListener('click', () => {
-            editTask(tasks_input_el,tasks_edit_el)
-            
+            editTask(tasks_input_el,tasks_edit_el);
         })
 
         tasks_completed_el.addEventListener('click', () => {
             if(confirm("Task Completed?")){
-                
+                list_cl.appendChild(transfer(list_cl,tasks));
                 list_el.removeChild(tasks_el);
             }
         })
@@ -54,6 +50,14 @@ window.addEventListener('load', () => {
     })
 })
 
+function create(tasks){
+        temp = document.createElement("input");
+        temp.classList.add("text");
+        temp.type = "text";
+        temp.value = tasks;
+        temp.setAttribute("readonly", "readonly");
+        return temp;
+}
 
 function editTask(inputElement,taskElement){
     if(taskElement.innerText == "EDIT"){
@@ -77,4 +81,33 @@ function createButton(inputName){
     temp.classList.add(inputName);
     temp.innerHTML = inputName.toUpperCase();
     return temp;
+}
+
+function transfer(list,temp){
+    
+    const tasks_el = createDiv("tasks");
+
+    const tasks_list_el = createDiv("content");
+
+    tasks_el.appendChild(tasks_list_el);
+
+    const tasks_input_el = create(temp);
+
+    tasks_list_el.appendChild(tasks_input_el);
+
+    const tasks_actions_el = createDiv("actions");
+
+    const tasks_delete_el = createButton("delete");
+
+    tasks_actions_el.appendChild(tasks_delete_el);
+
+    tasks_el.appendChild(tasks_actions_el);
+
+    tasks_delete_el.addEventListener('click', () => {
+        if(confirm("Task Completed?")){
+            list.removeChild(tasks_el);
+        }
+    })
+
+    return tasks_el;
 }
